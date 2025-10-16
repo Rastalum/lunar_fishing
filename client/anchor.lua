7eb178097499016f0424ff122de3b0aa3e2be2ad
Rastalum <rastalum@mailbox.org>
2025-10-16 23:49:46 +0200

    Update anchor.lua
    Add Anchor Notification
    Add Anchor Notify an Message Check

--- client/anchor.lua
+++ client/anchor.lua
@@ -11,7 +11,7 @@ lib.onCache('vehicle', function(vehicle)
         return
     end
     
-    if IsPedInAnyBoat(cache.ped) and IsPedDiving(cache.ped) then
+    if IsPedInAnyBoat(cache.ped) and GetPedInVehicleSeat(cache.vehicle, -1) == GetPlayerPed(-1) then
         local localeName = IsBoatAnchoredAndFrozen(vehicle) and 'raise_anchor' or 'anchor_boat'
         ShowUI(locale(localeName, keybind.currentKey), 'anchor')
         shown = true
@@ -24,16 +24,20 @@ keybind = lib.addKeybind({
     defaultKey = 'G',
     defaultMapper = 'keyboard',
     onReleased = function()
-        if not IsPedInAnyBoat(cache.ped)
-        or #GetEntityVelocity(cache.vehicle) > 3.0 then return end
+        if not IsPedInAnyBoat(cache.ped) or GetPedInVehicleSeat(cache.vehicle, -1) ~= GetPlayerPed(-1) then return end
+        if #GetEntityVelocity(cache.vehicle) > 3.0 and IsPedInAnyBoat(cache.ped) and GetPedInVehicleSeat(cache.vehicle, -1) == GetPlayerPed(-1) then
+			ShowNotification(locale('msg_anchor_to_fast'), 'error')
+			return end
 
         if IsBoatAnchoredAndFrozen(cache.vehicle) then
             SetBoatAnchor(cache.vehicle, false)
             ShowUI(locale('anchor_boat', keybind.currentKey), 'anchor')
+			ShowNotification(locale('msg_anchor_raised'), 'success')
         else
             SetBoatFrozenWhenAnchored(cache.vehicle, true)
             SetBoatAnchor(cache.vehicle, true)
             ShowUI(locale('raise_anchor', keybind.currentKey), 'anchor')
+			ShowNotification(locale('msg_anchor_lowered'), 'success')
         end
     end
 })
